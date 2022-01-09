8ec0d229cff1dae945aea5820ea79292455fc7a0
NobodyExpectsTheSpanishInquisition <s.miklaszewski1999@gmail.com>
2022-01-09 23:48:45 +0100

    clean symfony module and improve dockerfile

--- init.sh
+++ init.sh
@@ -8,6 +8,3 @@ SYMFONY_CONTAINER_TARGET_FOLDER_PATH=${SYMFONY_APP_TARGET_VOLUME}
 
 #set all permissions for app folder
 docker exec -it "$SYMFONY_CONTAINER_NAME" chmod 777 "$SYMFONY_CONTAINER_TARGET_FOLDER_PATH"
-
-# composer install
-docker exec -it "$SYMFONY_CONTAINER_NAME" composer install
